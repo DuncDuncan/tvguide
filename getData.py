8a1d8558c3399f6906282bd68f8535a6f7ee5775
DuncDuncan <77699076+DuncDuncan@users.noreply.github.com>
2022-01-20 12:59:27 +0000

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,9 +1,8 @@
 channels = ['Rai 1', 'Rai 2', 'Rai 3', 'Rete 4', 'Canale 5', 'Italia 1', 'La 7', 'TV 8', 'Nove', '20 Mediaset', 'Rai 4', 'Iris', 'Rai 5', 'Rai Movie', 'Rai Premium', 'Cielo', 'La 7d', 'La 5', 'Real Time', 'Focus', 'Giallo', 'Top Crime', 'Dmax', 'Rai Storia', 'Mediaset Extra', 'Italia 2', 'Blaze', 'Boing', 'Boomerang', 'Caccia & Pesca', 'Comedy Central', 'Crime Investigation', 'Dea Junior', 'Dea Kids', 'Discovery', 'Discovery Science', 'Eurosport 1', 'Eurosport 2', 'Food Network', 'Fox', 'Gambero Rosso', 'History', 'LaEffe', 'Nat Geo Wild', 'National Geographic', 'Nick Junior', 'Nickelodeon', 'Pesca & Caccia', 'Primafila 1', 'Primafila 2', 'Primafila 3', 'Primafila 4', 'Primafila 5', 'Rai Gulp', 'Rai Scuola', 'Rai Sport', 'Rai Sport +', 'Rai Yoyo', 'Rsi La1', 'Rsi La2', 'Sky Atlantic', 'Sky Cinema Action', 'Sky Cinema Collection', 'Sky Cinema Comedy', 'Sky Cinema Drama', 'Sky Cinema Due', 'Sky Cinema Family', 'Sky Cinema Romance', 'Sky Cinema Suspence', 'Sky Cinema Uno', 'Sky Sport', 'Sky Sport Arena', 'Sky Sport F1', 'Sky Sport Football', 'Sky Sport MotoGP', 'Sky Sport NBA', 'Sky Sport Uno', 'Sky Uno', 'Tg Com 24']
 
+
 import requests
-
 import xml.etree.ElementTree as ET
-
 
 
 def fetch():
@@ -31,10 +30,8 @@ def fetch():
   #f.close()
 
 
-
-
 def reduce(xml_data):
-  #print ('meow',xml_data[0:10])
+  print ('reduce start')
   #tree = ET.parse('data.txt')
   
   #tree = ET.parse(xml_data)
@@ -43,7 +40,7 @@ def reduce(xml_data):
 
   s0 = '<TV>'
   for channel in channels:
-    print (channel)
+    #print (channel)
     s = ''
     for x in root:
       if x.tag=="programme":
@@ -63,7 +60,7 @@ def reduce(xml_data):
   f.write(s0)
 
 
-  #print ('reduce end')
+  print ('reduce end')
  
 
 
